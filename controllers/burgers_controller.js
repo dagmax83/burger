30736a8c6afb6cdbca4daca4ab24f8b14e10c354
DAGMAX83 <DAGMAX983@gmail.com>
2018-12-08 17:22:41 -0600

    trying to get my hamburgers to show

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,7 +3,7 @@ var express = require("express");
 
 var router = express.Router();
 
-// Import the model (burger.js) to use its database functions.
+// Import the model (cat.js) to use its database functions.
 var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
@@ -17,33 +17,96 @@ router.get("/", function(req, res) {
   });
 });
 
-router.post("/api/burger", function(req, res) {
-  burger.insert(["burger_name"],  function(data) {
+router.post("/api/burgers", function(req, res) {
+  burger.create([
+    "name", "burger"
+  ], [
+    req.body.name, req.body.devoured
+  ], function(result) {
     // Send back the ID of the new quote
-    res.redirect("/");
+    res.json({ id: result.insertId });
   });
 });
 
-router.put("/api/burger/:id", function(req, res) {
+router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
-  burger.update(
-    {
-      devoured: true
-    },
-    condition,
-    function(data) {
-    releaseEvents.redirect('/');
-    
-          if (result.changedRows === 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      }
-      res.status(200).end();
+  console.log("condition", condition);
 
+  burger.update({
+    devoured: req.body.devoured
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
     }
-  );
+  });
+});
+
+router.delete("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  burger.delete(condition, function(result) {
+    if (result.affectedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
 });
 
 // Export routes for server.js to use.
 module.exports = router;
+
+
+// var express = require("express");
+
+// var router = express.Router();
+
+// // Import the model (burger.js) to use its database functions.
+// var burger = require("../models/burger.js");
+
+// // Create all our routes and set up logic within those routes where required.
+// router.get("/", function(req, res) {
+//   burger.all(function(data) {
+//     var hbsObject = {
+//       burgers: data
+//     };
+//     console.log(hbsObject);
+//     res.render("index", hbsObject);
+//   });
+// });
+
+// router.post("/api/burger", function(req, res) {
+//   burger.insert(["burger_name"],  function(data) {
+//     // Send back the ID of the new quote
+//     res.redirect("/");
+//   });
+// });
+
+// router.put("/api/burger/:id", function(req, res) {
+//   var condition = "id = " + req.params.id;
+
+//   burger.update(
+//     {
+//       devoured: true
+//     },
+//     condition,
+//     function(data) {
+//     releaseEvents.redirect('/');
+    
+//           if (result.changedRows === 0) {
+//         // If no rows were changed, then the ID must not exist, so 404
+//         return res.status(404).end();
+//       }
+//       res.status(200).end();
+
+//     }
+//   );
+// });
+
+// // Export routes for server.js to use.
+// module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -17,8 +17,14 @@ var burger = {
     orm.update("burgers", objColVals, condition, function(res) {
       cb(res);
     });
+  },
+  delete: function(condition, cb) {
+    orm.delete("burgers", condition, function(res) {
+      cb(res);
+    });
   }
 };
+
 
 // Export the database functions for the controller (burger_controller.js).
 module.exports = burger;
